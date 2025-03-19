0517fafe80dd4eaba4a155e613ca3921ba47f968
Shoaib Ansari <shoaib.ahmad.ansari@accenture.com>
2025-03-20 02:49:10 +0530

    push the examples

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Starter from './tutorial/1-useState/setup/1-error-example';
 function App() {
   return (
     <div className='container'>
       <h2>Advanced Tutorial</h2>
+      <Starter />
     </div>
   )
 }
--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  
+  const [title, setTitle] = useState('random title')
+  const handleClick = () => {
+    setTitle('Hello People')
+  };
+  return (
+    <React.Fragment>
+      <h2>{title}</h2>
+      <button type='button' className='btn' onClick={handleClick}>
+        change title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default ErrorExample;
